24dd1699bef6242e624399fa563e3afc3c5d3d98
Christopher Gerber <chriswgerber@gmail.com>
2014-03-12 18:24:30 -0500

    Moved more functions and blocks to child class. Cleared more from the functions.php and moved to separate pages for better organization. Added more widgets for page configuration.

--- wp-content/themes/reporter-child/functions.php
+++ wp-content/themes/reporter-child/functions.php
@@ -1,57 +1,73 @@
 <?php
+define('CHILDDIR', get_stylesheet_directory());
 /*
  *
  * Get the content position with the sidebar options
  *
  */
+function engine_content_position() {
 
-if (!function_exists('engine_content_position'))
-{
-    function engine_content_position() {
+    if( !is_admin() ) {
 
-        if( !is_admin() ) {
+        global $reporter_data;
 
-            global $reporter_data;
+        // Default
+        $sidebar_position = 'right';
+        $content_position = 'large-8 left';
 
-            // Default
-            $sidebar_position = 'right';
-            $content_position = 'large-8 left';
+        // Default sidebar positions
+        if( is_single() )
+            if( isset($reporter_data['post_sidebar_pos']) )
+                $sidebar_position = $reporter_data['post_sidebar_pos'];
 
-            // Default sidebar positions
-            if( is_single() )
-                if( isset($reporter_data['post_sidebar_pos']) )
-                    $sidebar_position = $reporter_data['post_sidebar_pos'];
+        if( is_page() )
+            if( isset($reporter_data['page_sidebar_pos']) )
+                $sidebar_position = $reporter_data['page_sidebar_pos'];
 
-            if( is_page() )
-                if( isset($reporter_data['page_sidebar_pos']) )
-                    $sidebar_position = $reporter_data['page_sidebar_pos'];
+        if( is_archive() )
+            if( isset($reporter_data['archive_sidebar_pos']) )
+                $sidebar_position = $reporter_data['archive_sidebar_pos'];
 
-            if( is_archive() )
-                if( isset($reporter_data['archive_sidebar_pos']) )
-                    $sidebar_position = $reporter_data['archive_sidebar_pos'];
+        // Override if sidebar position is set for post/page metabox
+        if( is_singular() ) {
 
-            // Override if sidebar position is set for post/page metabox
-            if( is_singular() ) {
+            $single_sidebar_position = get_post_meta(get_the_ID(), 'engine_sidebar_pos', TRUE);
 
-                $single_sidebar_position = get_post_meta(get_the_ID(), 'engine_sidebar_pos', TRUE);
-
-                if( $single_sidebar_position != '' )
-                    $sidebar_position = $single_sidebar_position;
-
-            }
-
-            if( $sidebar_position == 'right-sidebar' ) $content_position = 'large-8 left';
-            if( $sidebar_position == 'left-sidebar' ) $content_position = 'large-8 right';
-            if( $sidebar_position == 'no-sidebar' ) $content_position = 'large-12';
-
-            $output = $content_position;
-
-            return $output;
+            if( $single_sidebar_position != '' )
+                $sidebar_position = $single_sidebar_position;
 
         }
 
+        if( $sidebar_position == 'right-sidebar' ) $content_position = 'large-8 left';
+        if( $sidebar_position == 'left-sidebar' ) $content_position = 'large-8 right';
+        if( $sidebar_position == 'no-sidebar' ) $content_position = 'large-12';
+
+        $output = $content_position;
+
+        return $output;
+
     }
 }
+
+/**
+ * Add Theme parts after theme is created
+ * Makes Page builder Child-themeable.
+ */
+add_action( 'after_setup_theme', function() {
+
+    // add blocks
+    require_once( CHILDDIR . '/blocks/epg-stock-index-block.php');
+    require_once( CHILDDIR . '/blocks/epg-ad-position-block.php');
+    require_once( CHILDDIR . '/blocks/epg-ad-position-test-block.php');
+    require_once( CHILDDIR . '/blocks/aq-widgets-block.php');
+
+    // register blocks
+    aq_register_block('EPG_Stock_Index_Block');
+    aq_register_block('EPG_Ad_Position_Block');
+    aq_register_block('EPG_Ad_Position_Test_Block');
+    aq_register_block('AQ_Widgets_Block');
+
+}, 2 );
 
 /**
  *
@@ -61,8 +77,7 @@ if (!function_exists('engine_content_position'))
  * @param string $separator
  * @return string
  */
-
-function the_category_filter($thelist,$separator=' ') {
+function the_category_filter( $thelist, $separator=' ' ) {
     if(!defined('WP_ADMIN')) {
         //Category IDs to exclude
         //Excludes Uncategorized, Top Stories
@@ -87,113 +102,61 @@ function the_category_filter($thelist,$separator=' ') {
 }
 add_filter('the_category','the_category_filter', 10, 2);
 
-/**
- * Ad Position Widget
- * Adds a widget that takes an ad position's variables and creates a new ad position
- *
- */
-class WP_Widget_google_ads_position extends WP_Widget {
-
-    function __construct() {
-        $widget_ops = array('classname' => 'widget_google_ad_position', 'description' => __("A simple DoubleClick ad position.", 'reactor') );
-        parent::__construct('dbc-ad-position', __('Double Click Ad Position', 'reactor'), $widget_ops);
-        $this->alt_option_name = 'widget_dbc_ad_position';
-    }
-
-    function widget($args, $instance) {
-        ob_start();
-        extract($args);
-
-        if( empty($instance['title']) ){
-            $title = 'Ad Position';
-        }else{
-            $title = apply_filters('widget_title', $instance['title'], $instance, $this->id_base);
-        }
-
-        if ( $instance['adPositionId'] ) {
-            $content =  "\n" .'<script type="text/javascript">' .
-                "\n" . 'googletag.cmd.push(function() { googletag.display("' . $instance['adPositionId'] . '"); });' .
-                "\n" . '</script>';
-        } else {
-            $content = '';
-        }
-        $before_cmt = "\n<!-- $title -->\n";
-        $before_content = "\n" . '<div id="' .  $instance['adPositionId'] . '" class="adPosition" style="width:' .
-            $instance['width'] . 'px; height:' .  $instance['height'] . 'px;">' . "\n";
-        $after_content = "\n" . '</div>' . "\n";
-
-        ## Output - Position ID included
-        if ($content) :
-            $output_content =
-                $before_widget .
-                    $before_cmt .
-                    $before_content .
-                        $content .
-                    $after_content .
-                $after_widget;
-            ## Print the output
-            echo $output_content;
-        endif;
-
-        ob_flush();
-    }
-
-    function form( $instance ) {
-        // div comment
-        $title = isset($instance['title']) ? esc_attr( $instance['title'] ) : '';
-        // div id
-        $adPositionId = isset($instance['adPositionId']) ? esc_attr( $instance['adPositionId'] ) : '';
-        // position min-width
-        $width = isset($instance['width']) ? absint( $instance['width'] ) : '';
-        // position min-height
-        $height = isset($instance['height']) ? absint( $instance['height'] ) : '';
-        ?>
-        <p><label for="<?php echo $this->get_field_id('title'); ?>"><?php _e('Title:', 'engine'); ?></label>
-            <input class="widefat" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>"
-                type="text" value="<?php echo $title; ?>" placeholder="Comment from Code" /></p>
-        <p><label for="<?php echo $this->get_field_id('adPositionId'); ?>"><?php _e('Ad Position ID:', 'engine'); ?></label>
-            <input class="widefat" id="<?php echo $this->get_field_id('adPositionId'); ?>" name="<?php echo $this->get_field_name('adPositionId'); ?>"
-            type="text" value="<?php echo $adPositionId; ?>" placeholder="The ID of the div" /></p>
-        <div class="fatHalf">
-            <p><label for="<?php echo $this->get_field_id('width'); ?>"><?php _e('Position Width:', 'engine'); ?></label>
-            <input id="<?php echo $this->get_field_id('width'); ?>" name="<?php echo $this->get_field_name('width'); ?>"
-                type="number" value="<?php echo $width; ?>" placeholder="In Pixels" /></p>
-            <p><label for="<?php echo $this->get_field_id('height'); ?>"><?php _e('Position Height:', 'engine'); ?></label>
-            <input id="<?php echo $this->get_field_id('height'); ?>" name="<?php echo $this->get_field_name('height'); ?>"
-                type="number" value="<?php echo $height; ?>" placeholder="In Pixels" /></p>
-        </div>
-        <?php
-    }
-
-    function update( $new_instance, $old_instance ) {
-        $instance = $old_instance;
-        $instance['title'] = strip_tags($new_instance['title']);
-        $instance['adPositionId'] = strip_tags($new_instance['adPositionId']);
-        $instance['width'] = (int) $new_instance['width'];
-        $instance['height'] = (int) $new_instance['height'];
-        return $instance;
-    }
+/*
+Widget: Ad Position
+Description: Adds a widget that takes an ad position's variables and creates a new ad position
+*/
+require_once( CHILDDIR . '/widgets/epg-google-ad-position-widget.php');
+function epg_google_ad_position_widget_init() {
+    register_widget('epg_google_ad_position_widget');
 }
-
-function WP_Widget_google_ads_position_init() {
-    register_widget('WP_Widget_google_ads_position');
-}
-
-add_action('widgets_init', 'WP_Widget_google_ads_position_init');
-
-function archive_first_excerpt() {
-
-}
-
-
+add_action('widgets_init', 'epg_google_ad_position_widget_init');
 
 /*
- * Talk with theme create about supporting child blocks.
-//require_once( TEMPLATEPATH . '/library/theme-pagebuilder/aq-page-builder.php');
-if(class_exists('AQ_Page_Builder')) {
-    //include the block files
-    //require_once(AQPB_PATH . 'blocks/epg-stock-index-block.php');
-    //register the blocks
-    //aq_register_block('EPG_Stock_Index_Block');
+Plugin Name: Display Categories Widget
+Description: Display Categories Widget to display on your sidebar, this will get the title and category id
+Plugin URI: http://www.iteamweb.com/
+Version: 1.0
+Author: Suresh Baskaran
+License: GPL
+*/
+require_once( CHILDDIR . '/widgets/epg-display-categories-widget.php');
+function DisplayCategoriesWidget_init() {
+    register_widget('DisplayCategoriesWidget');
 }
+add_action('widgets_init', 'DisplayCategoriesWidget_init');
+
+
+/**
+ * Top menu
  */
+function register_top_menu() {
+    register_nav_menu('secondary-menu',__( 'Secondary Menu' ));
+}
+add_action( 'init', 'register_top_menu' );
+
+
+/**
+ * Additional widgets to be placed with Aqua Page Builder
+ * Will not display on site directly
+ */
+function register_on_page_widgets () {
+
+    $sidebars = array(
+        'category_sidebar' => __('Category Sidebar', 'engine')
+
+    );
+
+    foreach ($sidebars as $key => $value) {
+
+        register_sidebar(array('name'=> $value,
+            'id' => $key,
+            'before_widget' => '<li id="%1$s" class="widget %2$s">',
+            'after_widget' => '</li>',
+            'before_title' => '<h3 class="widget-title">',
+            'after_title' => '</h3>'
+        ));
+
+    }
+}
+add_action('init', 'register_on_page_widgets');

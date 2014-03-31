5bc47f8a2541c137e8cf8f441c80191280267e1a
Christopher Gerber <chriswgerber@gmail.com>
2014-03-31 13:29:04 -0500

    Fixed Calendar Widget
    Added HTML Replace page

--- wp-content/themes/reporter-child/blocks/block-functions.php
+++ wp-content/themes/reporter-child/blocks/block-functions.php
@@ -9,12 +9,12 @@ add_action( 'after_setup_theme', function() {
     require_once( CHILDDIR . '/blocks/epg-stock-index-block.php');
     require_once( CHILDDIR . '/blocks/epg-ad-position-block.php');
     require_once( CHILDDIR . '/blocks/aq-widgets-block.php');
-    require_once( CHILDDIR . '/blocks/epg-ai1ec-agenda-block.php');
+    require_once( CHILDDIR . '/blocks/epg-ai1ec-agenda-widget-block.php');
 
     // register blocks
     aq_register_block('EPG_Stock_Index_Block');
     aq_register_block('EPG_Ad_Position_Block');
     aq_register_block('AQ_Widgets_Block');
-    aq_register_block('epg_ai1ec_agenda_block');
+    aq_register_block('epg_ai1ec_agenda_widget_block');
 
 }, 2 );

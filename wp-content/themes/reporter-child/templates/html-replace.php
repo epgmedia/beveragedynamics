952d2f4ba5169f25941714cb44c197835e2331d4
Christopher Gerber <chriswgerber@gmail.com>
2014-04-04 16:37:36 -0500

    HTML Replace Script

--- wp-content/themes/reporter-child/templates/html-replace.php
+++ wp-content/themes/reporter-child/templates/html-replace.php
@@ -1,17 +1,55 @@
 <?php
-
 /*
- * Template Name: Do Not Use - Post/Excerpt HTML Clean
+ * Template Name: Admin Use - Database String Replace
  */
+/**
+ * @param $replaceString
+ * @param $newString
+ */
+function epg_sql_table_replace($replaceString, $newString) {
 
-header("Content-Type: text/plain");
+    echo "<h2>In these fields, <kbd>{$replaceString}</kbd> has been replaced with <kbd>{$newString}</kbd></h2>";
+    $t = 0;
+    while ($table = mysql_fetch_row($replaceString)) {
+        echo '<table style="margin-bottom:20px;">';
+        $fields_result = mysql_query("SHOW COLUMNS FROM " . $table[0]);
+        if (!$fields_result) {
+            echo '<tr><td colspan="2">Could not run query: ' . mysql_error() . '</td></tr>';
+            exit;
+        }
+        $i = 0;
+        if (mysql_num_rows($fields_result) > 0) {
+            echo "<tr><td colspan='2'><strong>Table: {$table[0]}</strong></td></tr>";
+            while ($field = mysql_fetch_assoc($fields_result)) {
+                if (stripos($field['Type'], "VARCHAR") !== false || stripos($field['Type'], "TEXT") !== false) {
+                    echo '<tr>';
+                    echo '<td style="padding-right:10px;">' . $field['Field'] . '</td>';
+                    $sql = "UPDATE " . $table[0] .
+                        " SET " . $field['Field'] . " = replace(" . $field['Field'] . ", '$string_to_replace', '$newString')";
+                    mysql_query($sql);
+                    echo '<td>' . mysql_affected_rows() . ' records updated.</td>';
+                    echo '</tr>';
+                    $i = $i + mysql_affected_rows();
+                }
 
-$host = "localhost";
-$username = "root";
-$password = "";
-$database = "mydatabase";
-$string_to_replace  = 'old.example.com';
-$new_string = 'new.example.com';
+            }
+        }
+        echo '<tr><td colspan="2">' . $i . ' total records updated.</td></tr>';
+        echo "</table>";
+        $t = $t + $i;
+    }
+
+    echo "$t changes made to the database";
+
+}
+
+$host               = "localhost";
+$username           = "beverage_user";
+$password           = "gB*7p3tkKN#6";
+$database           = "beverage_data";
+$string1            = 'cgerber-epg.ad.epgmediallc.com/projects/beverage-dynamics';
+$string2            = 'ec2-54-84-150-204.compute-1.amazonaws.com';
+$nString1           = 'www.beveragedynamics.com';
 
 // Connect to database server
 mysql_connect($host, $username, $password);
@@ -20,33 +58,16 @@ mysql_connect($host, $username, $password);
 mysql_select_db($database);
 
 // List all tables in database
-$sql = "SHOW TABLES FROM ".$database;
+$sql = "SHOW TABLES FROM " . $database;
 $tables_result = mysql_query($sql);
 
 if (!$tables_result) {
-  echo "Database error, could not list tables\nMySQL error: " . mysql_error();
-  exit;
+    echo "Database error, could not list tables\n\nMySQL error: " . mysql_error();
+    exit;
 }
 
-echo "In these fields '$string_to_replace' have been replaced with '$new_string'\n\n";
-while ($table = mysql_fetch_row($tables_result)) {
-  echo "Table: {$table[0]}\n";
-  $fields_result = mysql_query("SHOW COLUMNS FROM ".$table[0]);
-  if (!$fields_result) {
-      echo 'Could not run query: ' . mysql_error();
-      exit;
-  }
-  if (mysql_num_rows($fields_result) > 0) {
-      while ($field = mysql_fetch_assoc($fields_result)) {
-          if (stripos($field['Type'], "VARCHAR") !== false || stripos($field['Type'], "TEXT") !== false) {
-              echo "  ".$field['Field']."\n";
-              $sql = "UPDATE ".$table[0]." SET ".$field['Field']." = replace(".$field['Field'].", '$string_to_replace', '$new_string')";
-              mysql_query($sql);
-          }
-      }
-      echo "\n";
-  }
-}
+epg_sql_table_replace($string1, $nString1);
+
+epg_sql_table_replace($string2, $nString1);
 
 mysql_free_result($tables_result);
-?>
